8d3bfe7a8a4c3b54543fb21fcf06e62744897d14
Matteo Federico Zazzetta <fulminedipegasus@gmail.con>
2020-03-08 16:44:43 +0100

    Wrong commit

--- able/src/org/able/PythonBluetooth.java
+++ able/src/org/able/PythonBluetooth.java
@@ -8,15 +8,21 @@ import android.bluetooth.BluetoothGattDescriptor;
 
 interface PythonBluetooth
 {
-        public void on_error(int reason, String msg);
-        public void on_device(BluetoothDevice device, int rssi, byte[] record);
-        public void on_scan_started(boolean success);
-        public void on_scan_completed();
-        public void on_services(int status, List<BluetoothGattService> services);
-        public void on_characteristic_changed(BluetoothGattCharacteristic characteristic);
-        public void on_characteristic_read(BluetoothGattCharacteristic characteristic, int status);
-        public void on_characteristic_write(BluetoothGattCharacteristic characteristic, int status);
-        public void on_descriptor_read(BluetoothGattDescriptor descriptor, int status);
-        public void on_descriptor_write(BluetoothGattDescriptor descriptor, int status);
-        public void on_connection_state_change(int status, int state);
+    int REASON_NO_BLE = 1;
+    int REASON_NO_ADAPTER = 2;
+    int REASON_NOT_ENABLED = 3;
+    int REASON_DISCOVER_ERROR = 4;
+    int REASON_NO_SCANNER = 5;
+
+    public void on_error(int reason, String msg);
+    public void on_device(BluetoothDevice device, int rssi, byte[] record);
+    public void on_scan_started(boolean success);
+    public void on_scan_completed();
+    public void on_services(int status, List<BluetoothGattService> services);
+    public void on_characteristic_changed(BluetoothGattCharacteristic characteristic);
+    public void on_characteristic_read(BluetoothGattCharacteristic characteristic, int status);
+    public void on_characteristic_write(BluetoothGattCharacteristic characteristic, int status);
+    public void on_descriptor_read(BluetoothGattDescriptor descriptor, int status);
+    public void on_descriptor_write(BluetoothGattDescriptor descriptor, int status);
+    public void on_connection_state_change(int status, int state);
 }
